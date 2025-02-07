4411f5c7608d90d13a17c168ec8fa6c6fe1e827b
EvgenyGorG <evgenygorokhovg@yahoo.com>
2025-02-07 21:04:01 +0300

    Added a get_file_expansion function

--- picture_work_instruments.py
+++ picture_work_instruments.py
@@ -1,3 +1,6 @@
+from os.path import split, splitext
+from urllib.parse import urlparse, unquote
+
 import requests
 
 
@@ -7,3 +10,12 @@ def picture_download(picture_name, picture_url, images_file_path):
 
     with open(images_file_path + '\\' + picture_name, 'wb') as picture:
         picture.write(response.content)
+
+
+def get_file_expansion(url):
+    url_parts = urlparse(url)
+    url_address = url_parts[2]
+    url_name = unquote(split(url_address)[1])
+    file_expansion = splitext(url_name)[1]
+
+    return file_expansion
