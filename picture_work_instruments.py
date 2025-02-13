2c46e0ea5f2dee03b13d4b945d4dec9c621929c8
EvgenyGorG <evgenygorokhovg@yahoo.com>
2025-02-13 11:33:29 +0300

    Added relative paths

--- picture_work_instruments.py
+++ picture_work_instruments.py
@@ -8,7 +8,7 @@ def picture_download(picture_name, picture_url, images_file_path):
     response = requests.get(picture_url)
     response.raise_for_status()
 
-    with open(f'{images_file_path }\\{picture_name}', 'wb') as picture:
+    with open(f'{images_file_path }/{picture_name}', 'wb') as picture:
         picture.write(response.content)
 
 
