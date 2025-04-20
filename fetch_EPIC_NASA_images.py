aecd9212647c6a76caef7ff92a3b6c59be76e834
Evgeny <evgenygorokhovg@yahoo.com>
2025-04-20 22:09:44 +0300

    Added the ability to specify a number of images

--- fetch_EPIC_NASA_images.py
+++ fetch_EPIC_NASA_images.py
@@ -9,7 +9,7 @@ import requests
 from picture_work_instruments import download_picture, get_file_expansion
 
 
-def download_nasa_epic_images(api_key, images_file_path):
+def download_nasa_epic_images(api_key, images_file_path, number_of_images):
     epic_images_info = 'https://api.nasa.gov/EPIC/api/natural/images'
 
     payload = {
@@ -20,7 +20,9 @@ def download_nasa_epic_images(api_key, images_file_path):
     images_info_response.raise_for_status()
     images_info_response = images_info_response.json()
 
-    for page_number, page in enumerate(images_info_response, start=1):
+    images_info = images_info_response[:number_of_images]
+
+    for page_number, page in enumerate(images_info, start=1):
         file_name = page['image']
 
         image_date_time = page['date']
@@ -52,9 +54,17 @@ def main():
         default=Path(Path.cwd(), 'images')
     )
 
+    parser.add_argument(
+        '-n',
+        '--number_of_images',
+        type=str,
+        help='How many photos should upload?',
+        default=5
+    )
+
     args = parser.parse_args()
 
-    download_nasa_epic_images(nasa_api_key, args.directory)
+    download_nasa_epic_images(nasa_api_key, args.directory, args.number_of_images)
 
 
 if __name__ == '__main__':
